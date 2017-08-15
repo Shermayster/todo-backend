4b0f829925cea19fcd79982404c5651ffda7ce0a
pavelshermayster <sherm84@gmail.com>
2017-08-15 22:33:53 +0300

    commit

--- routes/app.js
+++ routes/app.js
@@ -3,33 +3,8 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-        if(err) {
-            return res.send('Error!');
-        }
-        res.render('node', { email: doc.email });
-    });
+   res.render('index');
 });
 
-
-router.post('/', function(req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Pavel',
-        lastName: 'Shermayster',
-        password: 'super-secret',
-        email: email
-    });
-    user.save(function(err, result) {
-        if(err) {
-            console.log('err', err);
-        }
-        if(result) {
-            console.log('result', result);
-        }
-        
-    });
-    res.redirect('/');
-});
 
 module.exports = router;
